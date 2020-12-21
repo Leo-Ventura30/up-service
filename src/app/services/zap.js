22403bbbf6b361f4f0c9b81781b7c1f2e118e4ab
leo-ventura30 <leonardo.ventura@outlook.com>
2020-12-20 23:33:55 -0300

    chore: export module

--- src/app/services/zap.js
+++ src/app/services/zap.js
@@ -1,7 +1,5 @@
-function mountLink(user) {
+export default function mountLink(user) {
   const textZap = `Olá ${user.nome}, só passando para confirmar o seu agendamento para o dia ${user.dia} as ${user.hora}`;
   const linkZap = `https://wa.me/5511${user.number}?text=${textZap}&app_absent=1`;
   return linkZap;
 }
-
-export default mountLink;
