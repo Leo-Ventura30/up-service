d68ed8a9d9f09ac72a67b95e8f73353b4a34ed47
leo-ventura30 <leonardo.ventura@outlook.com>
2021-01-27 00:08:51 -0300

    chore: reducer code

--- src/app/screens/Body/Section/Container/Content/Items/index.js
+++ src/app/screens/Body/Section/Container/Content/Items/index.js
@@ -7,65 +7,26 @@ import Picture from "../../../../../../assets/picture.svg";
 import Edit from "../../../../../../assets/edit.svg";
 import Bin from "../../../../../../assets/bin.svg";
 import api from "../../../../../../services/api";
-// class Items extends Component {
-//   state = {
-//     appointments: [],
-//   };
-
-//   render() {
-//     return (
-//       <Fragment>
-//         {!this.state.appointments[0] && <h1>Não há agendamentos</h1>}
-//         {this.state.appointments.map((e, k) => (
-//           <ItemStyle key={k}>
-//             <ItemIconStyle
-//               className="person-pic"
-//               src={Picture}
-//               alt="pic"
-//             ></ItemIconStyle>
-//             <ul>
-//               <li>
-//                 <p>{e.nome}</p>
-//               </li>
-//               <li>
-//                 <p>
-//                   <a href={mountLink(e)} target="_blank">
-//                     {e.number}
-//                   </a>
-//                 </p>
-//               </li>
-//               <li>
-//                 <p>{e.dia}</p>
-//               </li>
-//               <li>
-//                 <p>{e.hora}</p>
-//               </li>
-//               <li>
-//                 <p>{e.servico}</p>
-//               </li>
-//             </ul>
-//             <button>Finalizar</button>
-//             <ContentIconStyle>
-//               <img src={Edit} alt="pic"></img>
-//               <img src={Bin} alt="pic"></img>
-//             </ContentIconStyle>
-//           </ItemStyle>
-//         ))}
-//       </Fragment>
-//     );
-//   }
-// }
+import { Redirect } from "react-router-dom";
 
 export default class extends Component {
   state = {
     appointments: [],
   };
   async componentDidMount() {
-    const response = await api.get("/dashboard/appointments", {
-      headers: { "x-access-token": localStorage.getItem("token") },
-    });
-    this.setState({ appointments: response.data.appointments });
-    console.log(this.state.appointments[0]);
+    if (false == false) {
+      const response = await api.get("/dashboard/appointments", {
+        headers: { "x-access-token": localStorage.getItem("token") },
+      });
+      if (response.status === 500) {
+        console.log(0);
+        return <Redirect path="/" to="/" />;
+      } else {
+        this.setState({ appointments: response.data.appointments });
+      }
+    } else {
+      return <Redirect path="/" to="/" />;
+    }
   }
   render() {
     return (
