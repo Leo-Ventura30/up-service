74b20b7f2b0cd0f6efc4f17cdfddb447bda5ced5
leofventura98 <leonardo.ventura@outlook.com>
2021-09-09 23:15:28 -0300

    chore: refactor code

--- src/app/features/dashboard/containers/DashboardServices/index.js
+++ src/app/features/dashboard/containers/DashboardServices/index.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React from 'react';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import styled from 'styled-components'
-import {VerticalTabs} from '../../../../ui/VerticalTab'
-import { Card, CardHeader, Avatar, CardContent, Typography } from "@material-ui/core"
+import { TabPanel } from '../../../../ui/TabPanel';
+import FullCalendarComponent from '../../../../ui/FullCalendar'
 
-
-const PageWrapper = styled.div`
+const TabsContainer = styled.div`
+    background:#fff;
+    display:flex;
     height:100vh;
-`
+    max-height:115vh;
+    // max-width:45vh;
+    // width:40vh;
+    .Tabs-navigation{
+        border-right: 1px solid #aaa
+    }
 
-export const DashboardServicesContainer = () =>
-    <PageWrapper>
-        <VerticalTabs/>
-    </PageWrapper>
+` 
+export const DashboardServicesContainer = ({value, handleChange, initial_items}) => {
+
+  return (
+    <TabsContainer >
+        <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        className="Tabs-navigation"      
+        >
+            {/* <Tab label={INITIAL_ITEMS[0]}/>   
+            <Tab label={INITIAL_ITEMS[1]} /> */}
+             {initial_items.map(item=><Tab label={item} />)}   
+        </Tabs>
+        {/* <TabPanel value={INITIAL_ITEMS[value]} index={INITIAL_ITEMS[0]}>
+                {INITIAL_ITEMS[0]}...
+        </TabPanel> */}
+        <TabPanel value={initial_items[value]} index={initial_items[0]}>
+            <FullCalendarComponent/>
+        </TabPanel>
+        <TabPanel value={initial_items[value]} index={initial_items[1]}>
+            <FullCalendarComponent/>
+        </TabPanel>
+    </TabsContainer>
+  );
+}
--- src/app/features/dashboard/screens/DashboardServices/index.js
+++ src/app/features/dashboard/screens/DashboardServices/index.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { DashboardServicesContainer } from "../../containers/DashboardServices";
+const INITIAL_ITEMS = ["Meu calendário", "Meus clientes" , "Faturamento", "Criar serviço", "Meus serviços", "Adicionar produto", "Estoque", "Adicionar funcionário", "Meus funcionários"]
 
 export const DashboardServicesScreen = () => {
-    
-    return <DashboardServicesContainer/>
+    const [value, setValue] = useState(0);
+    const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+    return <DashboardServicesContainer value={value} initial_items={INITIAL_ITEMS} handleChange={handleChange} />
 }
